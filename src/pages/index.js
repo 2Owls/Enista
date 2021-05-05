d6ef352486f84ac735dd91ae999076a4a22e7b24
2Owls <hello@2owls.co.uk>
2021-05-05 17:12:10 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return  <div class="container">
+            <div class="box">
+              <div>
+                <img src="img/enista_logo_transparent.svg" alt="Enista" width="100%"/>
+              </div>
+            </div>
+          </div>
 }
